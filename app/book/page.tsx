cf67c75be051d62a72bbdf4799c99df60dfda480
gaoxiu <gaoxiu333@gmail.com>
2023-12-03 21:35:33 +0800

    feat: book

---
+++ app/book/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import React, { useRef } from "react";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
+import { Mesh } from "three";
+
+function MeshComponent() {
+  // 获取模型
+  const fileUrl = "/book/scene.gltf";
+  const mesh = useRef<Mesh>(null!);
+  const gltf = useLoader(GLTFLoader, fileUrl);
+
+  // 旋转动画
+//   useFrame(() => {
+//     mesh.current.rotation.y += 0.01;
+//   });
+
+  return (
+    <mesh ref={mesh}>
+      <primitive object={gltf.scene} />
+    </mesh>
+  );
+}
+
+export default function Page() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      {/* 画布 */}
+      <Canvas className="h-2xl w-2xl">
+        {/* 交互控制 */}
+        <OrbitControls />
+        {/* 灯光 */}
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <MeshComponent />
+      </Canvas>
+    </div>
+  );
+}
